9ef9d23300d5579686a3b6a1d8101ea27b0ee3aa
Emily Buckley <52759844+emilybuc@users.noreply.github.com>
2021-12-01 09:56:34 +0000

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameOverCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Replay(){
+        SceneManager.LoadScene(0);
+    }
+}
